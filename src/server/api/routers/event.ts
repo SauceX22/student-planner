a09a5549954d056279098f4501add906103bb9e4
SauceX <mahmoudzak4@gmail.com>
2024-03-31 16:48:37 +0300

    chore: add event table

---
+++ src/server/api/routers/event.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { events } from "@/server/db/schema";
+
+export const eventRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+});
