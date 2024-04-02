08ce2019307b322b10090331f96632032d8ee652
SauceX <mahmoudzak4@gmail.com>
2024-04-02 06:27:10 +0300

    config!: add prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,51 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 
-export default config;
+module.exports = {
+  endOfLine: "lf",
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  quoteProps: "as-needed",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/env(.*)$",
+    "^@/types/(.*)$",
+    "^@db/types/(.*)$",
+    "^@db/(.*)$",
+    "^@/config/(.*)$",
+    "^@lib/(.*)$",
+    "^@utils/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/pages/(.*)$",
+    "^@/app/(.*)$",
+    "^@/(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  tailwindConfig: "./tailwind.config.ts",
+  tailwindFunctions: ["clsx", "cn", "tw"],
+  plugins: [
+    require("@ianvs/prettier-plugin-sort-imports"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
+};
