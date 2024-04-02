8fb186660124f772d82ea3d8857007e4248145f0
SauceX <mahmoudzak4@gmail.com>
2024-04-02 06:30:15 +0300

    chore: change page bg colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { api } from "@/trpc/server";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#15162c] text-white">
       test
     </main>
   );
