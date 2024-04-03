62b14d1018a19e4bee2edccfb83c10f9371d1c36
SauceX <mahmoudzak4@gmail.com>
2024-04-03 17:06:03 +0300

    chore: sizing fix

--- src/components/events/animated-card.tsx
+++ src/components/events/animated-card.tsx
@@ -31,7 +31,7 @@ const AnimatedCard = ({ className, ...props }: Props) => {
       )}
       style={{ height: expanded ? "auto" : "fit-content" }}
       onTap={() => setExpanded(!expanded)}
-      whileHover={{ scale: 1.03 }}
+      whileHover={{ scale: 1.01 }}
       whileTap={{ scale: 0.98 }}
       transition={{
         duration: 0.1,
