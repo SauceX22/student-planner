a7e3b9df268e86e1e21269ab49f48e725ddad2b4
SauceX <mahmoudzak4@gmail.com>
2024-04-08 01:36:20 +0300

    feat: finish sidebar hover animation

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,21 +1,15 @@
-import { buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { Calendar } from "@/components/calendar/calendar";
 import { DashboardHeader } from "@/components/dashboard/header";
 import { DashboardShell } from "@/components/dashboard/shell";
-import AnimatedCard from "@/components/events/animated-card";
-import { cn } from "@/lib/utils";
 
-type Props = {};
-
-const HomePage = (props: Props) => {
+const HomePage = () => {
   return (
     <DashboardShell>
       <DashboardHeader heading="Home" text="" />
       <Separator className="mt-4 w-full" />
       <div className="flex h-full w-full items-center justify-center overflow-hidden px-6 py-4">
-        <AnimatedCard>
-          <p className="text-lg font-semibold">Drag me around!</p>
-        </AnimatedCard>
+        <Calendar />
       </div>
     </DashboardShell>
   );
--- src/components/dashboard/nav.tsx
+++ src/components/dashboard/nav.tsx
@@ -3,13 +3,14 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { AnimatePresence, m } from "framer-motion";
+import { useMouse } from "@uidotdev/usehooks";
+import { domAnimation, LazyMotion, m } from "framer-motion";
 import { debounce } from "lodash";
 import { ChevronRight } from "lucide-react";
 import type { User } from "next-auth";
 
 import { dashboardConfig } from "@/config/dashboard";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { UserAccountNav } from "@/components/user-account-nav";
 import { cn } from "@/lib/utils";
 
@@ -21,170 +22,96 @@ interface DashboardNavProps {
 const items = dashboardConfig.sidebarNav;
 
 const MotionLink = m(Link);
-const MotionUserAccountNav = m(UserAccountNav);
 
-const HOVER_THRESHOLD = 0.15 as const;
-const DEBOUNCE_TIME = 440 as const;
+const HOVER_THRESHOLD = 0.13;
+const DEBOUNCE_TIME = 600;
 
 export function DashboardNav({ user }: DashboardNavProps) {
   const path = usePathname();
   const [expanded, setExpanded] = useState(false);
+  const [mouse, componentRef] = useMouse();
 
   const handleHoverEnd = debounce(() => {
     setExpanded(false);
   }, DEBOUNCE_TIME); // Adjust debounce delay as needed
 
-  window.addEventListener("mousemove", (mouseEvent) => {
-    if (mouseEvent.clientX < window.innerWidth * HOVER_THRESHOLD) {
-      handleHoverEnd.cancel();
+  useEffect(() => {
+    handleHoverEnd.cancel();
+    if (mouse.x < window.innerWidth * HOVER_THRESHOLD) {
       setExpanded(true);
     } else {
       handleHoverEnd();
     }
-  });
+  }, [mouse.x]);
 
   if (!items?.length) {
     return null;
   }
 
   return (
-    <nav
-      className={cn(
-        "fixed left-0 top-0 z-50 m-6 flex h-[95%] w-[20%] flex-col items-center justify-start rounded-lg border-2 bg-white p-2 shadow-xl ease-out",
-        "transform transition duration-500",
-        expanded ? "-translate-x-0" : "-translate-x-[130%]"
-        // move the data-state stuff to use expanded state
-      )}>
-      <UserAccountNav user={user} />
-      <AnimatePresence>
-        {expanded && (
-          <div className="flex h-full w-full flex-col items-start justify-start gap-1 px-2 pb-2 pt-6">
-            {items.map((item, index) => {
-              if (
-                item.adminOnly
-                // TODO && session?.user.role !== "MANAGER"
-              ) {
-                return null;
-              }
+    <LazyMotion features={domAnimation}>
+      <m.nav
+        className="fixed left-0 top-0 z-50 m-6 flex h-[95%] w-[18%] flex-col items-center justify-start rounded-xl border-2 border-input bg-muted p-3 shadow-lg"
+        layout
+        initial={{
+          x: "-130%",
+          filter: "blur(2px)",
+        }}
+        animate={{
+          x: expanded ? "0" : "-130%",
+          filter: expanded ? "blur(0px)" : "blur(2px)",
+        }}
+        whileHover={{
+          x: "0",
+          filter: "blur(0px)",
+        }}
+        transition={{
+          duration: 0.2,
+          type: "spring",
+          stiffness: 400,
+          damping: 30,
+        }}>
+        <UserAccountNav user={user} />
+        <div className="flex h-full w-full flex-col items-start justify-start gap-1 px-2 pb-2 pt-6">
+          {items.map((item, index) => {
+            if (
+              item.adminOnly
+              // TODO && session?.user.role !== "MANAGER"
+            ) {
+              return null;
+            }
 
-              const Icon = item.icon ?? ChevronRight;
-              return (
-                item.href && (
-                  <MotionLink
-                    layout
-                    prefetch
-                    initial={{
-                      filter: "blur(2px)",
-                      opacity: 0,
-                    }}
-                    animate={{
-                      filter: "blur(0px)",
-                      opacity: 1,
-                    }}
-                    exit={{
-                      filter: "blur(2px)",
-                      opacity: 0,
-                    }}
-                    key={index}
-                    href={item.disabled ? "/dashboard" : item.href}
-                    whileHover={{ scale: 1.02 }}
-                    whileTap={{ scale: 0.98 }}
-                    transition={{
-                      duration: 0.1,
-                      type: "spring",
-                      stiffness: 700,
-                      damping: 30,
-                    }}
-                    className={cn(
-                      buttonVariants({ variant: "ghost" }),
-                      "group flex h-auto w-full items-center justify-start rounded-md border-none px-4 py-3 text-base font-medium text-foreground hover:bg-white hover:shadow-sm",
-                      path === item.href ? "bg-white shadow-sm" : "transparent",
-                      { "cursor-not-allowed opacity-80": item.disabled },
-                      { "mt-auto": index === items.length - 1 }
-                    )}>
-                    <Icon className="mr-2 h-4 w-4" />
-                    <span>{item.title}</span>
-                  </MotionLink>
-                )
-              );
-            })}
-          </div>
-        )}
-      </AnimatePresence>
-    </nav>
+            const Icon = item.icon ?? ChevronRight;
+            return (
+              item.href && (
+                <MotionLink
+                  layout
+                  prefetch
+                  key={index}
+                  href={item.disabled ? "/dashboard" : item.href}
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.98 }}
+                  transition={{
+                    duration: 0.1,
+                    type: "spring",
+                    stiffness: 700,
+                    damping: 30,
+                  }}
+                  className={cn(
+                    buttonVariants({ variant: "ghost" }),
+                    "group flex h-auto w-full items-center justify-start rounded-md border-none px-4 py-3 text-base font-medium text-foreground hover:bg-white hover:shadow-sm",
+                    path === item.href ? "bg-white shadow-sm" : "transparent",
+                    { "cursor-not-allowed opacity-80": item.disabled },
+                    { "mt-auto": index === items.length - 1 }
+                  )}>
+                  <Icon className="mr-2 h-4 w-4" />
+                  <span>{item.title}</span>
+                </MotionLink>
+              )
+            );
+          })}
+        </div>
+      </m.nav>
+    </LazyMotion>
   );
 }
-
-const Introduction = ({ focusChat }: { focusChat?: () => void }) => {
-  const [hideIntro, setHideIntro] = useState<boolean>(false);
-  const [ready, setReady] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (!hideIntro) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  }, [hideIntro]);
-
-  return (
-    <div
-      className={
-        "fixed inset-0 flex h-screen w-screen items-center justify-center transition-all " +
-        (hideIntro ? "pointer-events-none z-[52]" : "z-50 bg-white")
-      }>
-      <div className="flex max-w-md flex-col items-center">
-        <div
-          className={
-            "pointer-events-auto relative z-[52] flex gap-2 border bg-white px-3 py-2 transition-all duration-500 " +
-            (hideIntro
-              ? "w-56 -translate-y-[39vh] rounded-xl border-neutral-200 shadow-lg"
-              : "mb-8 w-32 translate-x-0 rounded-none border-white")
-          }>
-          <div
-            className={
-              "aspect-square rounded-full bg-[url('https://avatar.vercel.sh/rauchg?size=40')] bg-cover transition-all duration-500 " +
-              (hideIntro ? "w-12" : "w-24")
-            }></div>
-          {hideIntro && (
-            <div
-              className={
-                "absolute top-1/2 z-10 mb-2 -translate-y-1/2 truncate p-1.5 transition-all duration-500 " +
-                (hideIntro ? "left-[72px] opacity-100" : "opacity-0 blur-xl")
-              }>
-              <h2 className="font-medium">Florian's AI Clone</h2>
-              <p className="text-sm text-neutral-500">Online 24/7</p>
-            </div>
-          )}
-        </div>
-        {!ready ? (
-          <div
-            className={
-              "flex h-32 w-full flex-col items-center transition-all " +
-              (hideIntro ? "opacity-0 blur-lg" : "opacity-100 blur-none")
-            }>
-            <h1 className="mb-2 text-center text-2xl font-bold">
-              Talk to my AI clone
-            </h1>
-            <p className="mb-8 text-center text-neutral-500">
-              This AI is a clone of me, especially useful for employees who want
-              to ask me questions when I'm not around.
-            </p>
-            <Button
-              onClick={() => {
-                setHideIntro(true);
-                focusChat?.();
-                setTimeout(() => {
-                  setReady(true);
-                }, 500);
-              }}>
-              Get Started
-            </Button>
-          </div>
-        ) : (
-          <div className="h-32" />
-        )}
-      </div>
-    </div>
-  );
-};
