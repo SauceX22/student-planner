5142c8b9a0ef539cc1c6a32c7b4c8caba74191c4
SauceX <mahmoudzak4@gmail.com>
2024-04-12 17:10:08 +0300

    chore: calendar animation cleanup

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -6,25 +6,19 @@ import {
   endOfMonth,
   endOfWeek,
   format,
-  getWeeksInMonth,
   isBefore,
   isSameDay,
   startOfMonth,
   startOfToday,
   startOfWeek,
 } from "date-fns";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion";
 import { CalendarCheck2, ChevronLeft, ChevronRight } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { CardContent, CardHeader } from "@/components/ui/card";
 import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  MotionCardContent,
+  MotionCard,
   MotionCardDescription,
   MotionCardTitle,
 } from "@/components/ui/motion-card";
@@ -34,25 +28,28 @@ import { cn } from "@/lib/utils";
 import { api } from "@/trpc/client";
 
 const HEADER_MOVE_DISTANCE = 80;
-const BLOCK_MOVE_DISTANCE = 300;
+const BLOCK_MOVE_DISTANCE = 100;
 
 const headerVariants = {
   enter: (direction: number) => {
     return {
       x: direction > 0 ? HEADER_MOVE_DISTANCE : -HEADER_MOVE_DISTANCE,
       opacity: 0,
+      filter: "blur(2px)",
     };
   },
   center: {
     zIndex: 1,
     x: 0,
     opacity: 1,
+    filter: "blur(0px)",
   },
   exit: (direction: number) => {
     return {
       zIndex: 0,
       x: direction < 0 ? HEADER_MOVE_DISTANCE : -HEADER_MOVE_DISTANCE,
       opacity: 0,
+      filter: "blur(2px)",
     };
   },
 };
@@ -62,25 +59,28 @@ const blockVariants = {
     return {
       x: direction > 0 ? BLOCK_MOVE_DISTANCE : -BLOCK_MOVE_DISTANCE,
       opacity: 0,
+      filter: "blur(2px)",
     };
   },
   center: {
     zIndex: 1,
     x: 0,
     opacity: 1,
+    filter: "blur(0px)",
   },
   exit: (direction: number) => {
     return {
       zIndex: 0,
       x: direction < 0 ? BLOCK_MOVE_DISTANCE : -BLOCK_MOVE_DISTANCE,
       opacity: 0,
+      filter: "blur(4px)",
     };
   },
 };
 
-const MotionCalendarDay = motion(CalendarDay);
+const MotionCalendarDay = m(CalendarDay);
 
-const swipeConfidenceThreshold = 10000;
+const swipeConfidenceThreshold = 100000;
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity;
 };
@@ -120,140 +120,145 @@ export function Calendar() {
   };
 
   return (
-    <Card className="mb-auto w-fit select-none rounded-lg p-6 font-light shadow-xl">
-      <CardHeader className="mb-4 grid grid-cols-1 p-0">
-        <div className="flex justify-center overflow-hidden rounded-lg border border-neutral-800 p-6 shadow-sm">
-          <div className="flex flex-auto flex-col items-start justify-center gap-2">
-            <AnimatePresence initial={false} custom={direction} mode="wait">
-              <MotionCardTitle
-                className="my-auto flex-auto items-center justify-center text-xl font-bold md:text-5xl"
-                key={format(selectedDay, "MMM yyyy")}
-                layout
-                custom={direction}
-                variants={headerVariants}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{
-                  x: { duration: 0.1, ease: "easeOut" },
-                  opacity: { duration: 0.08 },
-                }}>
-                {format(selectedDay, "MMM yyyy")}
-              </MotionCardTitle>
-              <MotionCardDescription
-                key={format(selectedDay, "MMM d, yyyy")}
-                layout
-                custom={direction}
-                variants={headerVariants}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{
-                  x: { duration: 0.1, ease: "easeOut" },
-                  opacity: { duration: 0.08 },
-                }}>
-                {/* mention the actual selected day */}
-                {format(selectedDay, "MMM d, yyyy")}
-              </MotionCardDescription>
-            </AnimatePresence>
-          </div>
-
-          <div className="flex items-center justify-center gap-2">
-            <Button
-              size="icon"
-              variant="outline"
-              name="go-to-today"
-              className="h-fit w-fit rounded-full p-4"
-              onClick={() => paginate(0)}>
-              <CalendarCheck2 className="h-6 w-6" />
-            </Button>
-            <Button
-              size="icon"
-              variant="outline"
-              name="go-to-previous-month"
-              className="h-fit w-fit rounded-full p-4"
-              onClick={() => paginate(-1)}>
-              <ChevronLeft className="h-6 w-6" />
-            </Button>
-            <Button
-              size="icon"
-              variant="outline"
-              name="go-to-next-month"
-              className="h-fit w-fit rounded-full p-4"
-              onClick={() => paginate(1)}>
-              <ChevronRight className="h-6 w-6" />
-            </Button>
-          </div>
-        </div>
-      </CardHeader>
-      {/* === Headers for Days === */}
-      <motion.div
-        drag="x"
-        dragConstraints={{ left: 0, right: 0 }}
-        dragElastic={1}
-        className="mb-2 grid grid-cols-7 rounded-md border border-red-400 bg-red-300 p-1 py-1 text-center text-lg font-semibold leading-8">
-        <div className="px-2">S</div>
-        <div className="px-2">M</div>
-        <div className="px-2">T</div>
-        <div className="px-2">W</div>
-        <div className="px-2">T</div>
-        <div className="px-2">F</div>
-        <div className="px-2">S</div>
-      </motion.div>
-      {/* Calendar */}
-      {/* === Days === */}
-      <MotionCardContent
-        className="flex flex-row items-start justify-center overflow-hidden rounded-md border
-       border-orange-300 bg-orange-200 p-1 text-center text-lg font-semibold leading-8"
+    <LazyMotion features={domAnimation}>
+      <MotionCard
+        layout
         transition={{
           duration: 0.1,
           type: "spring",
           stiffness: 700,
           damping: 25,
-        }}>
-        {/* TODO: solve this by rendering the 2 adjacent month blocks and move each block in as needed */}
-        <AnimatePresence initial={false} custom={direction} mode="wait">
-          <motion.div
-            className="grid grid-cols-7"
-            key={page}
-            layout
-            custom={direction}
-            variants={blockVariants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            transition={{
-              x: { type: "spring", duration: 0.25 },
-              opacity: { duration: 0.2 },
-            }}
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
-              const swipe = swipePower(offset.x, velocity.x);
+        }}
+        className="mb-auto w-fit select-none overflow-hidden rounded-lg p-6 font-light shadow-xl">
+        <CardHeader className="mb-4 grid grid-cols-1 p-0">
+          <div className="flex justify-center overflow-hidden rounded-lg border border-neutral-800 p-6 shadow-sm">
+            <div className="flex flex-auto flex-col items-start justify-center gap-2">
+              <AnimatePresence initial={false} custom={direction} mode="wait">
+                <MotionCardTitle
+                  className="my-auto flex-auto items-center justify-center text-xl font-bold md:text-5xl"
+                  key={format(selectedDay, "MMM yyyy")}
+                  layout
+                  custom={direction}
+                  variants={headerVariants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { duration: 0.1, ease: "easeOut" },
+                    opacity: { duration: 0.08 },
+                    filter: { duration: 0.08 },
+                  }}>
+                  {format(selectedDay, "MMM yyyy")}
+                </MotionCardTitle>
+                <MotionCardDescription
+                  key={format(selectedDay, "MMM d, yyyy")}
+                  layout
+                  custom={direction}
+                  variants={headerVariants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { duration: 0.15, ease: "easeOut" },
+                    opacity: { duration: 0.08 },
+                    filter: { duration: 0.08 },
+                  }}>
+                  {/* mention the actual selected day */}
+                  {format(selectedDay, "MMM d, yyyy")}
+                </MotionCardDescription>
+              </AnimatePresence>
+            </div>
 
-              if (swipe < -swipeConfidenceThreshold) {
-                paginate(1);
-              } else if (swipe > swipeConfidenceThreshold) {
-                paginate(-1);
-              }
-            }}>
-            {daysBlock.map((day, dayIndx) => (
-              <MotionCalendarDay
-                key={day.toISOString()}
-                day={day}
-                layout
-                dayIndx={dayIndx}
-                firstDayOfMonth={startOfMonth(selectedDay)}
-                dayEvents={getDayEvents(day) ?? []}
-                className={cn("p-1.5 text-center text-sm", {
-                  grayscale: isLoadingMonthEvents,
-                })}
-              />
-            ))}
-          </motion.div>
-        </AnimatePresence>
-      </MotionCardContent>
-    </Card>
+            <div className="flex items-center justify-center gap-2">
+              <Button
+                size="icon"
+                variant="outline"
+                name="go-to-today"
+                className="h-fit w-fit rounded-full p-4"
+                onClick={() => paginate(0)}>
+                <CalendarCheck2 className="h-6 w-6" />
+              </Button>
+              <Button
+                size="icon"
+                variant="outline"
+                name="go-to-previous-month"
+                className="h-fit w-fit rounded-full p-4"
+                onClick={() => paginate(-1)}>
+                <ChevronLeft className="h-6 w-6" />
+              </Button>
+              <Button
+                size="icon"
+                variant="outline"
+                name="go-to-next-month"
+                className="h-fit w-fit rounded-full p-4"
+                onClick={() => paginate(1)}>
+                <ChevronRight className="h-6 w-6" />
+              </Button>
+            </div>
+          </div>
+        </CardHeader>
+        {/* === Headers for Days === */}
+        <m.div
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          className="mb-2 grid grid-cols-7 rounded-md border border-red-400 bg-red-300 p-1 py-1 text-center text-lg font-semibold leading-8">
+          <div className="px-2">S</div>
+          <div className="px-2">M</div>
+          <div className="px-2">T</div>
+          <div className="px-2">W</div>
+          <div className="px-2">T</div>
+          <div className="px-2">F</div>
+          <div className="px-2">S</div>
+        </m.div>
+        {/* Calendar */}
+        {/* === Days === */}
+        <CardContent className="borderborder-orange-300 flex flex-row items-start justify-center overflow-hidden rounded-md bg-orange-200 p-1 text-center text-lg font-semibold leading-8">
+          {/* TODO: solve this by rendering the 2 adjacent month blocks and move each block in as needed */}
+          <AnimatePresence initial={false} custom={direction} mode="wait">
+            <m.div
+              className="grid grid-cols-7"
+              key={page}
+              layout
+              custom={direction}
+              variants={blockVariants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { duration: 0.13, ease: "easeOut" },
+                opacity: { duration: 0.1 },
+                filter: { duration: 0.1 },
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
+
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}>
+              {daysBlock.map((day, dayIndx) => (
+                <MotionCalendarDay
+                  key={day.toISOString()}
+                  day={day}
+                  layout
+                  dayIndx={dayIndx}
+                  firstDayOfMonth={startOfMonth(selectedDay)}
+                  dayEvents={getDayEvents(day) ?? []}
+                  className={cn("p-1.5 text-center text-sm", {
+                    grayscale: isLoadingMonthEvents,
+                  })}
+                />
+              ))}
+            </m.div>
+          </AnimatePresence>
+        </CardContent>
+      </MotionCard>
+    </LazyMotion>
   );
 }
